217c7edb777d8a2790ff610014cabdd170e02390
lichen <18410132788@163.com>
2021-08-19 16:46:42 +0800

    登陆

---
+++ src/pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="box">登陆</div>
+        <div class="box">{{aaa}}</div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            aaa:'denglu'
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+.box{
+    width:100px;
+}
+</style>
